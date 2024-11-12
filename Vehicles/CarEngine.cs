bca808dedadf11ee5af7e7bf88328c5fd448648a
Metajjj <joolsjannaway@gmail.com>
2024-11-12 11:15:59 +0000

    t1-5

--- Vehicles/CarEngine.cs
+++ Vehicles/CarEngine.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
-    internal class CarEngine
+    internal class CarEngine : Engine
     {
+        public CarEngine(int horsePower=10, FuelTypes ft=FuelTypes.Diesel) : base(horsePower, ft)
+        {
+
+        }
     }
 }
