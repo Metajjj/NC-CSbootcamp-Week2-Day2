3df22f07109518501515fca7e31a3a2b8906d420
sz-99 <zhengsusan3@gmail.com>
2024-11-12 11:14:02 +0000

    add task 6 to 12

--- Vehicles/CarEngine.cs
+++ Vehicles/CarEngine.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Vehicles.Engine;
 
 namespace Vehicles
 {
-    internal class CarEngine
+    internal class CarEngine : Engine
     {
+        public CarEngine(int horsePower = 10, FuelTypes ft = FuelTypes.Diesel) : base(horsePower, ft)
+        {
+
+        }
     }
 }
--- Vehicles/Vehicles.cs
+++ Vehicles/Vehicles.cs
@@ -9,5 +9,35 @@ namespace Vehicles
     internal abstract class Vehicles
     {
         public string Make, Model; public int Speed;
+
+        public Engine EngineType;
+
+        public Vehicles(string make, string model, Engine engineType) 
+        {
+            this.Make = make;
+            this.Model = model;
+            this.EngineType = engineType;
+        }
+
+        public void StartEngine()
+        {
+            EngineType.Start();
+        }
+
+        public void Drive()
+        {
+            if(EngineType.Running)
+            {
+                Accelerate();
+
+                return;
+            }
+            Console.WriteLine($"{Make} {Model} is not running.");
+
+        }
+
+        protected abstract void Accelerate();
+        
+        
     }
 }
