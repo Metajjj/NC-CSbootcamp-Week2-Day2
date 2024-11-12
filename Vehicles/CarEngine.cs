b0dddb9528091c7408796eb496c82646f78643dd
Metajjj <joolsjannaway@gmail.com>
2024-11-12 11:18:16 +0000

    t1-5

--- Vehicles/CarEngine.cs
+++ Vehicles/CarEngine.cs
@@ -9,13 +9,12 @@ namespace Vehicles
 {
     internal class CarEngine : Engine
     {
-<<<<<<< HEAD
+
         public CarEngine(int horsePower=10, FuelTypes ft=FuelTypes.Diesel) : base(horsePower, ft)
-=======
-        public CarEngine(int horsePower = 10, FuelTypes ft = FuelTypes.Diesel) : base(horsePower, ft)
->>>>>>> 3df22f07109518501515fca7e31a3a2b8906d420
         {
 
         }
+
+        
     }
 }
